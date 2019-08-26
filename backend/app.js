a8441099385ebf3597a5f1df071abc38d9238511
rmaghsoudi <romy_mag@outlook.com>
2019-08-26 16:26:43 -0400

    connecting backend to frontend

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,33 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('first middleware');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+     'Origin, X-Requested-With, Accpet');
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS")
   next();
 });
 
-app.use((req, res, next) => {
-  res.send('Hello from the other siiiiideeee');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: "kjsadbfn24nj",
+      title: "first server side post",
+      content: "my neme server"
+    },
+    {
+      id: "rhqwj312k",
+      title: "second server side post",
+      content: "my neme json"
+    },
+  ];
+  res.status(200).json({
+    message: 'Posts Fetched Successfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
