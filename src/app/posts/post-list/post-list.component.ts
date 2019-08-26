c6701e739c5e068507c31e1de62c94d6f3bcf53d
rmaghsoudi <romy_mag@outlook.com>
2019-08-26 13:53:31 -0400

    added posts service

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,13 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit {
   // posts = [
   //   {title: 'First Post', content: 'This is the first post\'s content'},
   //   {title: 'Second Post', content: 'This is the second post\'s content'},
@@ -15,4 +16,9 @@ export class PostListComponent {
   // ];
   @Input() posts: Post[] = [];
 
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+  }
 }
