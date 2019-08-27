6da2d99e5d815c836b5e5dcb16114f7f570dfbba
rmaghsoudi <romy_mag@outlook.com>
2019-08-27 18:19:19 -0400

    creating and connecting db

--- backend/app.js
+++ backend/app.js
@@ -1,35 +1,53 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+//returns a promise, hence we can use the .thens, you should account for the failures, the "test" is the db name
+mongoose.connect("mongodb+srv://romy:kWqc6ftWcBx3kv3v@cluster0-iq6jj.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true})
+  .then(() => {
+    console.log("Connected to DB!");
+  })
+  .catch(() => {
+    console.log("DB connection failed!");
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Headers',
-     'Origin, X-Requested-With, Accpet');
+    'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PATCH, DELETE, OPTIONS")
   next();
 });
 
-app.use('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: "kjsadbfn24nj",
-      title: "first server side post",
-      content: "my neme server"
-    },
-    {
-      id: "rhqwj312k",
-      title: "second server side post",
-      content: "my neme json"
-    },
-  ];
-  res.status(200).json({
-    message: 'Posts Fetched Successfully!',
-    posts: posts
+app.post('/api/posts', (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
   });
+  post.save();
+  res.status(201).json({
+    message: 'Post added successfully'
+  });
+});
+
+app.get('/api/posts', (req, res, next) => {
+  Post.find()
+    .then(documents => {
+      res.status(200).json({
+        message: 'Posts Fetched Successfully!',
+        posts: documents
+      });
+    });
+
 });
 
 module.exports = app;
