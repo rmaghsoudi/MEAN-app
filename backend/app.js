fd0f060b6cc442ca9a839f528f3ffad6d98272a2
rmaghsoudi <romy_mag@outlook.com>
2019-09-03 12:47:59 -0400

    organizing routes

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Post = require('./models/post');
+const postsRoutes = require("./routes/posts");
 
 const app = express();
 //returns a promise, hence we can use the .thens, you should account for the failures, the "test" is the db name
@@ -28,60 +28,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/api/posts', (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save().then(result => {
-    res.status(201).json({
-      message: 'Post added successfully',
-      postId: result._id
-    });
-  });
-
-});
-
-app.get('/api/posts', (req, res, next) => {
-  Post.find()
-    .then(documents => {
-      res.status(200).json({
-        message: 'Posts Fetched Successfully!',
-        posts: documents
-      });
-    });
-});
-
-app.get('/api/posts/:id', (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({ message: 'Post not found!' });
-    }
-  })
-
-});
-
-app.patch("/api/posts/:id", (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  });
-  //updateOne takes 2 arguments, the id of the element and the new object you want to store
-  Post.updateOne({ _id: req.params.id}, post).then(result => {
-    console.log(result);
-    res.status(200).json({ message: "Update Successful!"});
-  });
-});
-
-// the wildcard is called a dynamic path segment ":"
-app.delete('/api/posts/:id', (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then(result => {
-    console.log(result);
-    res.status(200).json({ message: "Post Deleted!"});
-  })
-});
+// defining the route that'll use your imported routes
+app.use("/api/posts", postsRoutes);
 
 module.exports = app;
